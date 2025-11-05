9f482c255cbc438d6a7a54e315c9c8ae209b933c
mark chen <lazzyjessie@gmail.com>
2025-11-05 17:57:09 +0800

    feat: add `ComposableArchitecture` library

--- App/Package.swift
+++ App/Package.swift
@@ -13,11 +13,17 @@ let package = Package(
             targets: ["App"]
         ),
     ],
+    dependencies: [
+        .package(url: "https://github.com/pointfreeco/swift-composable-architecture", exact: "1.23.0"),
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "App"
+            name: "App",
+            dependencies: [
+                .product(name: "ComposableArchitecture", package: "swift-composable-architecture")
+            ]
         ),
         .testTarget(
             name: "AppTests",
