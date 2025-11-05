f06f3b113425ef8a2902b21885a5ce0ba3f27dad
mark chen <lazzyjessie@gmail.com>
2025-11-05 17:35:48 +0800

    feat: project init

---
+++ Audio Trimmer/Audio_TrimmerApp.swift
@@ -0,0 +1,18 @@
+//
+//  Audio_TrimmerApp.swift
+//  Audio Trimmer
+//
+//  Created by 陳琮諺 on 2025/11/5.
+//
+
+import SwiftUI
+import App
+
+@main
+struct Audio_TrimmerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
